5b4eeb5af6b1d99ce62665aeb347a904129d5d53
advanc3dUA <fash1k@gmail.com>
2023-01-04 12:33:32 +0100

    Added outlets & actions

--- SignUpFormExample/SignUpTableViewController.swift
+++ SignUpFormExample/SignUpTableViewController.swift
@@ -6,84 +6,50 @@
 //
 
 import UIKit
+import Combine
+
+// SIGN UP FORM RULES
+// - email address must be valid (contain @ and .)
+// - password must be at least 8 characters
+// - password cannot be "password"
+// - password confirmation must match
+// - must agree to terms
+// - BONUS: color email field red when invalid, password confirmation field red when it doesn't match the password
+// - BONUS: email address must remove spaces, lowercased
 
 class SignUpTableViewController: UITableViewController {
-
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var emailAddressField: FormTextField!
+    @IBOutlet weak var passwordField: FormTextField!
+    @IBOutlet weak var passwordConfirmationField: FormTextField!
+    @IBOutlet weak var agreeTermsSwitch: UISwitch!
+    @IBOutlet weak var signUpButton: BigButton!
+    
+    //MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    //MARK: - Actions
+    
+    @IBAction func emailDidChanged(_ sender: FormTextField) {
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    @IBAction func passwordDidChanged(_ sender: FormTextField) {
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    @IBAction func passwordConfirmationDidChanged(_ sender: FormTextField) {
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    @IBAction func agreeSwitchDidChanged(_ sender: UISwitch) {
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    @IBAction func signUpTapped(_ sender: BigButton) {
+        let alert = UIAlertController(title: "Welcome", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+        present(alert, animated: true)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
