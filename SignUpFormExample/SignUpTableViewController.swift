4f425b4d15a8c0c15b0953de28ad112dba3a7afe
advanc3dUA <fash1k@gmail.com>
2023-01-05 11:44:38 +0100

    Email check implemented

--- SignUpFormExample/SignUpTableViewController.swift
+++ SignUpFormExample/SignUpTableViewController.swift
@@ -27,23 +27,56 @@ class SignUpTableViewController: UITableViewController {
     @IBOutlet weak var agreeTermsSwitch: UISwitch!
     @IBOutlet weak var signUpButton: BigButton!
     
+    //MARK: - Subjects
+    private var emailSubject = CurrentValueSubject<String, Never>("")
+    private var passwordSubject = CurrentValueSubject<String, Never>("")
+    private var passwordConfirmationSubject = CurrentValueSubject<String, Never>("")
+    private var agreeTermsSubject = CurrentValueSubject<Bool, Never>(false)
+    private var cancellables: Set<AnyCancellable> = []
+    
     //MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        formIsValid
+            .assign(to: \.isEnabled, on: signUpButton)
+            .store(in: &cancellables)
     }
+    
+    //MARK: - Checking methods
+    private func emailIsValid(_ email: String) -> Bool {
+        email.contains("@") && email.contains(".")
+    }
+    
+    //MARK: - Publishers
+    private var formIsValid: AnyPublisher<Bool, Never> {
+        emailIsValid
+    }
+    
+    private var emailIsValid: AnyPublisher<Bool, Never> {
+        emailSubject
+            .map { [weak self] in self?.emailIsValid($0) }
+            .replaceNil(with: false)
+            .eraseToAnyPublisher()
+    }
+    
     
     //MARK: - Actions
     
     @IBAction func emailDidChanged(_ sender: FormTextField) {
+        emailSubject.send(emailAddressField.text ?? "")
     }
     
     @IBAction func passwordDidChanged(_ sender: FormTextField) {
+        passwordSubject.send(passwordField.text ?? "")
     }
     
     @IBAction func passwordConfirmationDidChanged(_ sender: FormTextField) {
+        passwordConfirmationSubject.send(passwordConfirmationField.text ?? "")
     }
     
     @IBAction func agreeSwitchDidChanged(_ sender: UISwitch) {
+        agreeTermsSubject.send(agreeTermsSwitch.isEnabled)
     }
     
     @IBAction func signUpTapped(_ sender: BigButton) {
